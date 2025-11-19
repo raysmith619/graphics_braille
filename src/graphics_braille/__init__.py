c986b916061ec2bcf3117af9b1ae68a3e93922ce
Charles R Smith (Ray) <raysmith@alum.mit.edu>
2025-11-19 10:33:18 -0500

    Supporting  sister Doc, gb_exercises

--- src/graphics_braille/__init__.py
+++ src/graphics_braille/__init__.py
@@ -1,11 +1,18 @@
 #__init__.py    30Oct2025  crs
 # Command line scripts for graphics_braille
+import shutil
+import pathlib
+
 from graphics_braille import *
+
+def main_function():
+    print("Installation Run")
 
 def gbhelp():
     print("""
           gb, gbhelp - List graphics-braille commands
           gbreadme - Display README.md
+          gbfiles - add user files to user desktop
           gbtest - Do simple program test
           """)
 
@@ -13,5 +20,20 @@ def gbreadme():
     import os
     os.system("distinfo_readme graphics_braille")
 
+def gbfiles():
+    """ Download user files
+    """
+    source_dir = "exercises"
+    desktop = pathlib.Path.home() / 'Desktop'
+    user_dir = desktop
+    try:
+        # Copy the directory tree, allowing existing directories
+        shutil.copytree(source_dir, user_dir, dirs_exist_ok=True)
+        print(f"Successfully copied '{source_dir}' to '{user_dir}'.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
+    
+    
 def gbtest():
     import graphics_braille.z_show_square_loop_colors_braille
---
+++ src/graphics_braille/__main__.py
@@ -0,0 +1,3 @@
+#__main__.py    12Nov2025  crs
+#Install auto setup
+print(f"Auto install: \n\n{__file__}\n\n")
